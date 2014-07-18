24a3ee228755a6a73f3474457b2497d6d99d0229
ocowchun <ocowchun@gmail.com>
2014-07-18 18:04:49 +0800

    New: add precompile js

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Bigpork
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+        config.assets.precompile += %w( pages/welcome_index.js )
+    config.assets.precompile += %w( pages/star_index.js )
   end
 end
