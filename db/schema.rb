105b5238334f4c0443663c6a5aef6feba96bc65f
ocowchun <ocowchun@gmail.com>
2014-07-18 23:18:06 +0800

    New: AddAvatarToItems

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718144852) do
+ActiveRecord::Schema.define(version: 20140718151744) do
 
   create_table "items", force: true do |t|
     t.string   "name"
     t.integer  "shop_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "items", ["shop_id"], name: "index_items_on_shop_id"
